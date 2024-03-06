cb2887de3ce4e01f0db3073ba1343405da6e5c92
Judi-legend <judilegend2@gmail.com>
2024-03-06 15:30:05 +0300

    fix navbar

--- client/web/src/pages/home.page.jsx
+++ client/web/src/pages/home.page.jsx
@@ -1,7 +1,11 @@
 import NavBar from "../components/nav.bar";
+import Hero from "../components/Hero";
 
-export default function HomePage(){
+export default function HomePage() {
     return (
-        <NavBar/>
+        <>
+            <NavBar />
+            <Hero />
+        </>
     )
 }
