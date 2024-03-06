99d6268ce085bf996831f6fb318f61415f06e0bd
Clavien317 <Claviennambinina511@gmail.com>
2024-03-06 17:38:48 +0300

    upload file

--- server/api/routes/pub-route.js
+++ server/api/routes/pub-route.js
@@ -1,20 +1,14 @@
-/** @format */
+const {createPub, reagir, commentaire, createAlert, notification} = require("../controllers/pub-controller")
+const router = require("express").Router()
+const upload = require("../middleware/uploads")
 
-const {
-  createPub,
-  reagir,
-  commentaire,
-  createAlert,
-  notification,
-} = require("../controllers/pub-controller");
-const router = require("express").Router();
 
-router.post("/create-pub", createPub);
-router.post("/reagir", reagir);
-router.post("/commentaire", commentaire);
-router.post("/evenement", createAlert);
-router.post("/notification", notification);
 
-module.exports = router;
+router.post("/create-pub",upload.single("cheminFichier"),createPub)
+router.post("/reagir",reagir)
+router.post("/commentaire",commentaire)
+router.post("/evenement",upload.single("cheminFichier"),createAlert)
+router.post("/notification",notification)
+
 
 module.exports = router;
