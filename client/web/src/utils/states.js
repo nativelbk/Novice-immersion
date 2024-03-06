c81d60e92414d67fb31d76c7ca5385a97211776d
Nativel <nativelbk@gmail.com>
2024-03-07 02:03:09 +0300

    add auth

--- client/web/src/utils/states.js
+++ client/web/src/utils/states.js
@@ -1,6 +1,12 @@
-import {atom} from "recoil"
+/** @format */
+
+import { atom } from "recoil";
 
 export const registerCompenentState = atom({
-    key : "registerComponent",
-    default : "Login"
-})
+  key: "registerComponent",
+  default: "Login",
+});
+export const authenticatedState = atom({
+  key: "authorized",
+  default: false,
+});
