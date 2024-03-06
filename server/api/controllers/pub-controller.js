e82e1914532f5c1aca84a9b866d522514c559db8
Nativel <nativelbk@gmail.com>
2024-03-06 15:35:11 +0300

    refactor the code

--- server/api/controllers/pub-controller.js
+++ server/api/controllers/pub-controller.js
@@ -1,40 +1,49 @@
-const pub = require("../models/publication-model")
-const reaction = require("../models/reaction-model")
-const coms = require("../models/comment-model")
-const event = require("../models/alert-model")
-const Notification = require("../models/notification-model")
+/** @format */
 
+const pub = require("../models/publication-model");
+const reaction = require("../models/reaction-model");
+const coms = require("../models/comment-model");
+const event = require("../models/alert-model");
+const Notification = require("../models/notification-model");
 
-const createPub =async(req,res)=>
-{
-    const result = await pub.create(req.body)
-    res.status(201).json({success:true,"message":"Created successfull","pub":result})
-}
+const createPub = async (req, res) => {
+  const result = await pub.create(req.body);
+  res.status(201).json({ success: true, pub: result });
+};
 
-const reagir=async(req,res)=>
-{
-    const react = await reaction.create(req.body)
-    res.status(201).json({success:true,message:"Created successfull",rect:react})
-}
+const reagir = async (req, res) => {
+  const react = await reaction.create(req.body);
+  res.status(201).json({ success: true, rect: react });
+};
 
-const commentaire =async(req,res)=>
-{
-    const comment = await coms.create(req.body)
-    res.status(201).json({success:true,message:"commented successfullity :)",comment:comment})
-}
+const commentaire = async (req, res) => {
+  const comment = await coms.create(req.body);
+  res.status(201).json({
+    success: true,
+    comment: comment,
+  });
+};
 
-const createAlert=async(req,res)=>
-{
-    const alert = await event.create(req.body)
-    res.status(201).json({success:true,message:"pub of event is successfullity :)",alert:alert})
-}
+const createAlert = async (req, res) => {
+  const alert = await event.create(req.body);
+  res
+    .status(201)
+    .json({
+      success: true,
+      message: "pub of event is successfullity :)",
+      alert: alert,
+    });
+};
 
-const notification=async(req,res)=>
-{
-    const notif =await Notification.create(req.body)
-    res.status(201).json({success:true,message:"notification successfullity :)",notif:notif})
+const notification = async (req, res) => {
+  const notif = await Notification.create(req.body);
+  res
+    .status(201)
+    .json({
+      success: true,
+      message: "notification successfullity :)",
+      notif: notif,
+    });
+};
 
-}
-
-
-module.exports = {createPub,reagir,commentaire,createAlert,notification}
+module.exports = { createPub, reagir, commentaire, createAlert, notification };
--- server/api/routes/pub-route.js
+++ server/api/routes/pub-route.js
@@ -1,13 +1,18 @@
-const {createPub, reagir, commentaire, createAlert, notification} = require("../controllers/pub-controller")
-const router = require("express").Router()
+/** @format */
 
+const {
+  createPub,
+  reagir,
+  commentaire,
+  createAlert,
+  notification,
+} = require("../controllers/pub-controller");
+const router = require("express").Router();
 
+router.post("/create-pub", createPub);
+router.post("/reagir", reagir);
+router.post("/commentaire", commentaire);
+router.post("/evenement", createAlert);
+router.post("/notification", notification);
 
-router.post("/create-pub",createPub)
-router.post("/reagir",reagir)
-router.post("/commentaire",commentaire)
-router.post("/evenement",createAlert)
-router.post("/notification",notification)
-
-
-module.exports = router
+module.exports = router;
