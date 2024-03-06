c39a3781a3fd7348283ed43df07158d5c90a56ed
Nativel <nativelbk@gmail.com>
2024-03-06 17:05:28 +0300

    Refactor the code

--- server/api/routes/pub-route.js
+++ server/api/routes/pub-route.js
@@ -1,14 +1,19 @@
-const {createPub, reagir, commentaire, createAlert, notification} = require("../controllers/pub-controller")
-const router = require("express").Router()
-const upload = require("../middleware/uploads")
+/** @format */
 
+const {
+  createPub,
+  reagir,
+  commentaire,
+  createAlert,
+  notification,
+} = require("../controllers/pub-controller");
+const router = require("express").Router();
+const upload = require("../middleware/uploads");
 
-
-router.post("/create-pub",upload.single("cheminFichier"),createPub)
-router.post("/reagir",reagir)
-router.post("/commentaire",commentaire)
-router.post("/evenement",upload.single("cheminFichier"),createAlert)
-router.post("/notification",notification)
-
+router.post("/create-pub", upload.single("cheminFichier"), createPub);
+router.post("/reagir", reagir);
+router.post("/commentaire", commentaire);
+router.post("/evenement", upload.single("cheminFichier"), createAlert);
+router.post("/notification", notification);
 
 module.exports = router;
