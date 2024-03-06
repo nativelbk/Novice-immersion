80782ebf3cf7752ae8e5c46f679f21c019d4005b
Nativel <nativelbk@gmail.com>
2024-03-06 15:35:11 +0300

    refactor the code

--- server/api/routes/pub-route.js
+++ server/api/routes/pub-route.js
@@ -1,11 +1,16 @@
-const {createPub, reagir, commentaire, createAlert} = require("../controllers/pub-controller")
-const router = require("express").Router()
+/** @format */
 
+const {
+  createPub,
+  reagir,
+  commentaire,
+  createAlert,
+} = require("../controllers/pub-controller");
+const router = require("express").Router();
 
+router.post("/create-pub", createPub);
+router.post("/reagir", reagir);
+router.post("/commentaire", commentaire);
+router.post("/evenement", createAlert);
 
-router.post("/create-pub",createPub)
-router.post("/reagir",reagir)
-router.post("/commentaire",commentaire)
-router.post("/evenement",createAlert)
-
-module.exports = router
+module.exports = router;
