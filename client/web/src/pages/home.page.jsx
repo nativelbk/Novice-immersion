ef7c63c1ba36dce53a7348663b9a88ef324c9cd3
Raly04 <nyainaraly@gmail.com>
2024-03-06 22:50:15 +0300

    clubs

--- client/web/src/pages/home.page.jsx
+++ client/web/src/pages/home.page.jsx
@@ -1,12 +1,14 @@
-import About from "../components/about";
+import About from "../components/parcours";
 import Description from "../components/description";
 import NavBar from "../components/nav.bar";
+import { Club } from "../components/club";
 
 export default function HomePage() {
   return (
     <>
       <NavBar />
       <Description/>
+      <Club/>
     </>
   );
 }
