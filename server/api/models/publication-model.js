ac2850a34c1824f144ea87fd07fc0678e9c75a7a
Nativel <nativelbk@gmail.com>
2024-03-06 11:49:53 +0300

    add publication-model

---
+++ server/api/models/publication-model.js
@@ -0,0 +1,25 @@
+/** @format */
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const schema = new mongoose.Schema(
+  {
+    descrition: {
+      type: String,
+      required: [true, "Please provide a description"],
+      trim: true,
+    },
+    path: {
+      type: String,
+    },
+    tag: {
+      type: [String],
+    },
+    category: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", schema);
