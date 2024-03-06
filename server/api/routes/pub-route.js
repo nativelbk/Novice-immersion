757ac91f1ead08f6eb9c454e492b243d39eacd23
Clavien317 <Claviennambinina511@gmail.com>
2024-03-06 14:45:52 +0300

    commenter pub

--- server/api/routes/pub-route.js
+++ server/api/routes/pub-route.js
@@ -1,10 +1,11 @@
-const {createPub, reagir} = require("../controllers/pub-controller")
+const {createPub, reagir, commentaire} = require("../controllers/pub-controller")
 const router = require("express").Router()
 
 
 
 router.post("/create-pub",createPub)
 router.post("/reagir",reagir)
+router.post("/commentaire",commentaire)
 
 
 module.exports = router
