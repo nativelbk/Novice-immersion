829f22a0be1836efd250eb47f9f08bfba9a8d12f
Clavien317 <Claviennambinina511@gmail.com>
2024-03-07 01:12:26 +0300

    no life

--- client/web/src/main.jsx
+++ client/web/src/main.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import App from "./App.jsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
 
 
 
-
+const router = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element: <App />
+    }
+  ]
+)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
+    {/* <NextUIProvider> */}
+      <RouterProvider router={router} />
+    {/* </NextUIProvider> */}
   </React.StrictMode>
 );
