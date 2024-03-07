339297f8045aed7b032526c2a75a858db9344fc8
Nativel <nativelbk@gmail.com>
2024-03-07 08:42:27 +0300

    salut

--- client/web/src/components/login.tsx
+++ client/web/src/components/login.tsx
@@ -30,7 +30,8 @@ export default function Login() {
     if (data.data) {
       setAuthorization(true);
       localStorage.setItem("token", data.data.token);
-      navigate("/dashboard");
+      localStorage.setItem("id", data.data.id);
+
       navigate("/dashboard");
     } else {
       setAuthorization(false);
@@ -111,20 +112,20 @@ export default function Login() {
             type="submit"
           >
             <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-secondary" />
-              <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                GitHub
-              </span>
-              <BottomGradient />
-            </button>
-            <button
-              className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-              type="submit"
-            >
-              <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-secondary" />
-              <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                Google
-              </span>
-              <BottomGradient />
+            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+              GitHub
+            </span>
+            <BottomGradient />
+          </button>
+          <button
+            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+            type="submit"
+          >
+            <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-secondary" />
+            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+              Google
+            </span>
+            <BottomGradient />
             <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-secondary" />
             <span className="text-neutral-700 dark:text-neutral-300 text-sm">
               GitHub
--- server/api/controllers/auth-controller.js
+++ server/api/controllers/auth-controller.js
@@ -1,25 +1,30 @@
-require('express-async-errors')
-const {StatusCodes} = require('http-status-codes')
-const model = require('../models/auth-model')
-const {BadRequest,Unauthenticated,NotFound} = require('../errors/index')
+/** @format */
 
+require("express-async-errors");
+const { StatusCodes } = require("http-status-codes");
+const model = require("../models/auth-model");
+const { BadRequest, Unauthenticated, NotFound } = require("../errors/index");
 
-const login = async (req,res)=>{
-    const {email,password} = req.body
-    if(!email || !password )throw new BadRequest('Please provide an email or password')
-    const user = await model.findOne({email:email})
-    if(!user)throw new NotFound(`No user with email: ${email} found`)
-    const isPasswordMatch = user.compare(password)
-    if(!isPasswordMatch) throw new Unauthenticated('Password does not match')
-    const token = user.createToken()
-    res.status(StatusCodes.OK).json({success:true,token:token})
-}
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    throw new BadRequest("Please provide an email or password");
+  const user = await model.findOne({ email: email });
+  if (!user) throw new NotFound(`No user with email: ${email} found`);
+  const isPasswordMatch = user.compare(password);
+  if (!isPasswordMatch) throw new Unauthenticated("Password does not match");
+  const token = user.createToken();
+  res
+    .status(StatusCodes.OK)
+    .json({ success: true, token: token, id: user._id });
+};
 
-const register = async (req,res)=>
-{
-    const user = await model.create(req.body)
-    const token = user.createToken()
-    res.status(StatusCodes.CREATED).json({success:true,token:token})
-}
+const register = async (req, res) => {
+  const user = await model.create(req.body);
+  const token = user.createToken();
+  res
+    .status(StatusCodes.CREATED)
+    .json({ success: true, token: token, id: user._id });
+};
 
-module.exports = {login,register}
+module.exports = { login, register };
