6677584497f1f3dec7fdf65d5dd48d1a8a2ffbdd
Raly04 <nyainaraly@gmail.com>
2024-03-07 11:41:22 +0300

    jdgvhwfef

--- client/web/src/pages/dashboard.page.jsx
+++ client/web/src/pages/dashboard.page.jsx
@@ -1,12 +1,24 @@
 import PublicationList from "../components/publication.list";
 import RigthBox from "../components/rigth.box";
 import SideBar from "../components/side.bar";
+import Orientation from "../components/Orientation";
+import { useRecoilState } from "recoil";
+import { dashMenuState } from "../utils/states";
+
+const components = {
+    PublicationList,
+    Orientation
+}
 
 export default function DashboarPage() {
+    const dashMenuItem = useRecoilState(dashMenuState) 
+
+    const DynamicComponent = components[dashMenuItem[0]] || <div>NOt FOund</div>
+
     return (
         <div className=" grid grid-cols-5 h-screen">
             <SideBar />            
-            <PublicationList/>
+            <DynamicComponent/>
             <RigthBox/>
         </div>
     )
