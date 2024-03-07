d34aabd14bdb8cdc1bc172c700459e714f4532b4
Raly04 <nyainaraly@gmail.com>
2024-03-07 06:53:55 +0300

    home

--- client/web/src/components/publication.list.jsx
+++ client/web/src/components/publication.list.jsx
@@ -2,10 +2,10 @@ import Publication from "./publication.box";
 
 export default function PublicationList() {
   return (
-    <div className=" col-span-3 h-screen overflow-y-auto">
-        <Publication />
-        <Publication />
-        <Publication />
+    <div className=" col-span-3 h-screen overflow-y-auto px-12 relative">
+      <Publication />
+      <Publication />
+      <Publication />
     </div>
   );
 }
