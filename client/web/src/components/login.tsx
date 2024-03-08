31a02b96dde3eec4b739c6faf93c1bc6fba811a1
Nativel <nativelbk@gmail.com>
2024-03-08 21:17:05 +0300

    fix bug

--- client/web/src/components/login.tsx
+++ client/web/src/components/login.tsx
@@ -1,10 +1,5 @@
 /** @format */
 
-import {
-  IconBrandGithub,
-  IconBrandGoogle,
-  IconBrandOnlyfans,
-} from "@tabler/icons-react";
 import React, { ChangeEvent, useState } from "react";
 import { Label } from "./label";
 import { Input } from "./input";
@@ -13,13 +8,14 @@ import axios from "axios";
 import { useRecoilState } from "recoil";
 import { authenticatedState } from "../utils/states";
 import { useNavigate } from "react-router-dom";
+import { IconBrandGithub, IconBrandGoogle } from "@tabler/icons-react";
 
 export default function Login() {
   const navigate = useNavigate();
 
   const [login, setLogin] = useState<{
-    email: "";
-    password: "";
+    email: string;
+    password: string;
   }>({ email: "", password: "" });
 
   const [authorization, setAuthorization] = useRecoilState(authenticatedState);
@@ -27,78 +23,80 @@ export default function Login() {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(login);
-    const data = await axios.post("http://localhost:5000/api/v1/login", login);
-    console.log(data);
-    if (data.data) {
-      setAuthorization(true);
-      localStorage.setItem("token", data.data.token);
-      navigate("/dashboard");
-      navigate("/dashboard");
-    } else {
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5000/api/v1/login",
+        login
+      );
+      console.log(data);
+      if (data) {
+        setAuthorization(true);
+        localStorage.setItem("token", data.token);
+        navigate("/dashboard");
+      } else {
+        setAuthorization(false);
+      }
+    } catch (error) {
+      console.error("Login failed:", error);
       setAuthorization(false);
     }
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setLogin((val) => {
-      return {
-        ...val,
-        [e.target.name]: e.target.value,
-      };
-    });
+    const { name, value } = e.target;
+    setLogin((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
-  return (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input">
-      <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-        Bienvenu chez-nous 
-      </h2>
 
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-    <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-      Welcome back
-    </h2>
-    <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-      Login to aceternity if you can because we don&apos;t have a login flow
-      yet
-    </p>
-    <form className="my-8" onSubmit={handleSubmit}>
-      <LabelInputContainer>
-        <Label htmlFor="email">E-mail</Label>
-        <Input
-          onChange={handleChange}
-          id="email"
-          placeholder="ralyandrynyainakadmiel@gmail.com"
-          type="text"
-          name="email"
-          required
-        />
-      </LabelInputContainer>
-      <LabelInputContainer className="mt-4">
-        <Label htmlFor="password">Password</Label>
-        <Input
-          onChange={handleChange}
-          id="password"
-          placeholder="••••••••"
-          name="password"
-          type="password"
-          required
-        />
-      </LabelInputContainer>
+      <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+        Welcome back
+      </h2>
+      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+        Login to aceternity if you can because we don&apos;t have a login flow
+        yet
+      </p>
+      <form className="my-8" onSubmit={handleSubmit}>
+        <LabelInputContainer>
+          <Label htmlFor="email">E-mail</Label>
+          <Input
+            onChange={handleChange}
+            id="email"
+            placeholder="ralyandrynyainakadmiel@gmail.com"
+            type="text"
+            name="email"
+            required
+          />
+        </LabelInputContainer>
+        <LabelInputContainer className="mt-4">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            onChange={handleChange}
+            id="password"
+            placeholder="••••••••"
+            name="password"
+            type="password"
+            required
+          />
+        </LabelInputContainer>
 
-      <button
-        className="bg-gradient-to-br relative group/btn mt-4 text-back bg-secondary w-full rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-        type="submit"
-      >
-        Login &rarr;
-        <BottomGradient />
-      </button>
+        <button
+          className="bg-gradient-to-br relative group/btn mt-4 text-back bg-secondary w-full rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          type="submit"
+        >
+          Login &rarr;
+          <BottomGradient />
+        </button>
+
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
         <div className="flex flex-col space-y-4">
           <button
             className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit"
+            type="button"
           >
             <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-secondary" />
             <span className="text-neutral-700 dark:text-neutral-300 text-sm">
@@ -108,7 +106,7 @@ export default function Login() {
           </button>
           <button
             className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit"
+            type="button"
           >
             <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-secondary" />
             <span className="text-neutral-700 dark:text-neutral-300 text-sm">
@@ -144,7 +142,3 @@ export const LabelInputContainer = ({
     </div>
   );
 };
-
-function setAuthorization(arg0: boolean) {
-  throw new Error("Function not implemented.");
-}
